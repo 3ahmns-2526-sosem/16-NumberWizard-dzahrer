b2fd2f2a127d290d83483690449da38c95c43bf8
Dominik Barley <dominik.zahrer08@htl-salzburg.ac.at>
2026-06-10 14:57:50 +0200

    add lower button logic

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -3,30 +3,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Number Range")]
     [SerializeField] private int min = 1;
     [SerializeField] private int max = 100;
-
-    [Header("UI")]
     [SerializeField] private TMP_Text guessText;
+
     [SerializeField] private int guess;
 
     private void Start()
     {
         CalculateGuess();
     }
+
     public void Higher()
     {
         min = guess + 1;
         CalculateGuess();
     }
+
+    public void Lower()
+    {
+        max = guess - 1;
+        CalculateGuess();
+    }
+
     private void CalculateGuess()
     {
         guess = (min + max) / 2;
-
-        if (guessText != null)
-        {
-            guessText.text = guess.ToString();
-        }
+        guessText.text = guess.ToString();
     }
 }
