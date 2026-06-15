a55cfffed86e65482be08d2170ee77a0fba0214a
Dominik Barley <dominik.zahrer08@htl-salzburg.ac.at>
2026-06-15 09:10:44 +0200

    forgot to create issue 5

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -1,27 +1,50 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int min = 1;
     [SerializeField] private int max = 100;
+
+    [Header("UI")]
     [SerializeField] private TMP_Text guessText;
+    [SerializeField] private TMP_Text messageText;
+
+    [Header("Buttons")]
+    [SerializeField] private Button higherButton;
+    [SerializeField] private Button lowerButton;
+    [SerializeField] private Button correctButton;
+
     [SerializeField] private int guess;
 
     private void Start()
     {
         CalculateGuess();
+        messageText.text = "";
     }
+
     public void Higher()
     {
         min = guess + 1;
         CalculateGuess();
     }
+
     public void Lower()
     {
         max = guess - 1;
         CalculateGuess();
     }
+
+    public void Correct()
+    {
+        messageText.text = "I guessed your number!";
+
+        higherButton.interactable = false;
+        lowerButton.interactable = false;
+        correctButton.interactable = false;
+    }
+
     private void CalculateGuess()
     {
         guess = (min + max) / 2;
